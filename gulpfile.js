8eb0882d9a237d9be2e9c53400be687b72ac1bb2
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-09-07 15:25:30 +0600

    feat(compile-blocks): add task for less2css

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@
 var gulp = require('gulp');
 var join = require('path').join;
 var basename = require('path').basename;
+var dirname = require('path').dirname;
 var through = require('through2');
 var sequence = require('run-sequence');
+var less = require('gulp-less');
 
 var prefix = 'node_modules/bootstrap/less'
 
@@ -15,6 +17,17 @@ var destInLevel = function(folder) {
       .pipe(gulp.dest(join(folder, blockName)))
       .on('end', cb);
   });
+}
+
+var compile = function(folder) {
+  return through.obj(function(file, enc, cb) {
+    gulp.src(file.path)
+      .pipe(less())
+      .pipe(gulp.dest(dirname(file.path)))
+      .on('end', cb);
+
+      return cb();
+    });
 }
 
 gulp.task('copy-less', function(cb) {
@@ -36,4 +49,8 @@ gulp.task('copy-docs-yamlconfig', function() {
 
 gulp.task('copy-docs-site', function() {
   return gulp.src('./node_modules/bootstrap/docs/**').pipe(gulp.dest('./docs/'));
+});
+
+gulp.task('compile-blocks', function() {
+  return gulp.src(['core/**/*.less']).pipe(compile());
 });
