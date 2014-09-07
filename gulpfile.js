ad1a9f2eac0c28f566b8d8177996d38fa2b6ab10
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-09-07 14:55:47 +0600

    refactor(copy-yamconfig): rename copy-yamlconfig

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('copy-variables', function() {
   return gulp.src([join(prefix, 'variables.less')]).pipe(gulp.dest('./core/variables/'));
 });
 
-gulp.task('copy-yamlconfig', function() {
+gulp.task('copy-docs-yamlconfig', function() {
   return gulp.src('./node_modules/bootstrap/_config.yml').pipe(gulp.dest('./'));
 });
 
