be14c650deb4484ca36b7e88cff91b23fa17f5a4
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-09-08 04:08:48 +0600

    refactor(gulpfile): big refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,32 @@ var dirname = require('path').dirname;
 var through = require('through2');
 var sequence = require('run-sequence');
 var less = require('gulp-less');
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
+var del = require('del');
 
-var levels = ['settings', 'reset'];
+var levels = ['settings', 'reset', 'glyphicons'];
+
+/**
+ * Clean all
+ */
+
+gulp.task('clean', function(cb) {
+  del(levels.concat('fonts', 'docs', '_config.yml'), cb);
+});
 
 /**
  * Copy all the stuff
  */
+gulp.task('copy', function(cb) {
+  sequence('clean', ['copy-less', 'copy-docs'], 'compile-blocks', cb)
+});
+
+/**
+ * Copy blocks
+ */
 gulp.task('copy-less', function(cb) {
-  sequence(['copy-settings', 'copy-reset'], 'compile-blocks', cb)
+  sequence(['copy-settings', 'copy-reset', 'copy-glyphicons'], 'compile-blocks', cb)
 });
 
 /**
@@ -21,7 +39,7 @@ gulp.task('copy-less', function(cb) {
  */
 gulp.task('compile-blocks', function() {
   var postfix = function(item) { return join(item, '*/*.less'); }
-  return gulp.src(levels.map(postfix)).pipe(compile());
+  return gulp.src(levels.slice(1).map(postfix)).pipe(compile());
 });
 
 
@@ -29,15 +47,22 @@ gulp.task('compile-blocks', function() {
  * # Settings section
  */
 gulp.task('copy-settings', function(cb) {
-  sequence(['copy-settings-blocks', 'copy-mixins'], cb)
+  sequence(['copy-settings-blocks', 'copy-mixins', 'copy-fonts'], cb)
 });
 
 gulp.task('copy-settings-blocks', function() {
-  return gulp.src(['variables.less', 'mixins.less'].map(prefix)).pipe(destInLevel('settings'));
+  return gulp.src(['variables.less', 'mixins.less'].map(prefix))
+    .pipe(replace('../fonts', '../../fonts'))
+    .pipe(rename(prependFilename))
+    .pipe(gulp.dest('settings'));
 });
 
 gulp.task('copy-mixins', function() {
   return gulp.src(['mixins/**.less'].map(prefix)).pipe(gulp.dest('settings/mixins/mixins'));
+});
+
+gulp.task('copy-fonts', function() {
+  return gulp.src(['node_modules/bootstrap/fonts/**']).pipe(gulp.dest('fonts'));
 });
 
 
@@ -45,9 +70,20 @@ gulp.task('copy-mixins', function() {
  * # Reset section
  */
 gulp.task('copy-reset', function(cb) {
-  return gulp.src(['normalize.less', 'print.less'].map(prefix)).pipe(destInLevel('reset'));
+  return gulp.src(['normalize.less', 'print.less'].map(prefix))
+    .pipe(rename(prependFilename))
+    .pipe(gulp.dest('reset'));
 });
 
+
+/**
+ * # Glyph section
+ */
+gulp.task('copy-glyphicons', function(cb) {
+  return gulp.src(['glyphicons.less'].map(prefix))
+    .pipe(rename(prependFilename))
+    .pipe(gulp.dest('glyphicons'));
+});
 
 /**
  * Docs
@@ -72,23 +108,40 @@ var prefix = function(item) {
   return join('node_modules/bootstrap/less', item);
 };
 
-var destInLevel = function(folder) {
-  return through.obj(function(file, enc, cb) {
-    var blockName = basename(file.path, '.less');
-
-    gulp.src([file.path])
-      .pipe(gulp.dest(join(folder, blockName)))
-      .on('end', cb);
-  });
-}
-
 var compile = function(folder) {
   return through.obj(function(file, enc, cb) {
     gulp.src(file.path)
+      .pipe(importSettings())
       .pipe(less())
       .pipe(gulp.dest(dirname(file.path)))
       .on('end', cb);
+  });
+};
 
-      return cb();
-    });
+var prependFilename = function(path) { path.dirname += '/' + path.basename; };
+
+var importSettings = function() {
+  return through.obj(function(file, enc, cb) {
+    var contents = file.contents.toString('utf8');
+    var prefix = [
+      '// Core variables and mixins',
+      '@import "../../settings/variables/variables.less";',
+      '@import "../../settings/mixins/mixins.less";'
+    ].join('\n');
+
+    file.contents = new Buffer(prefix + '\n\n' + contents);
+
+    this.push(file);
+    return cb();
+  });
+}
+
+var debug = function() {
+  return through.obj(function(file, enc, cb) {
+    console.log('Processing ' + file.relative);
+    console.log(file.contents.toString('utf8'));
+
+    this.push(file);
+    return cb();
+  });
 }
