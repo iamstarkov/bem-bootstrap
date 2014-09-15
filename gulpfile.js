073ff488eff4b6b6ee212d489bfe8e3469b155c0
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-09-15 20:23:52 +0600

    refactor(tasks/type): add tasks for copying type styles

--- gulpfile.js
+++ gulpfile.js
@@ -163,10 +163,11 @@ gulp.task('copy-scaffolding', function() {
 
     .pipe(extractSelector(/\.sr-only {[\s\S]*?}/g, 'core-css', 'sr-only'))
     .pipe(replace(/\.sr-only {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
     .pipe(extractSelector(/\.sr-only-focusable {[\s\S]*}/g, 'core-css', 'sr-only'))
     .pipe(replace(/\.sr-only-focusable {[\s\S]*}/g, ''))
-
     .pipe(wait(500))
+
     .pipe(rename(prependFilename))
     .pipe(gulp.dest('levels/scaffolding'));
 });
@@ -175,14 +176,14 @@ gulp.task('copy-core-css', function(cb) {
   sequence('copy-type', 'copy-grid', 'copy-buttons', cb);
 });
 
-gulp.task('copy-type', function() {
+gulp.task('copy-type', function(cb) {
 
   return gulp.src(['type.less'].map(prefix))
     .pipe(wait(500))
     /**
      * Headings
      */
-    .pipe(replace(/(,\s)/g, ', '))
+    .pipe(replace(/(,[\s]*)/g, ', '))
     .pipe(replace(/[,]* \.h(1|2|3|4|5|6)/g, ''))
 
     // h1, h2, h3, h4, h5, h6
@@ -190,21 +191,21 @@ gulp.task('copy-type', function() {
     .pipe(replace(/h1, h2, h3, h4, h5, h6/g, '.h1, .h2, .h3, .h4, .h5, .h6'))
     .pipe(extractSelector(/\.h1, \.h2, \.h3, \.h4, \.h5, \.h6[\s\S]*?}\n}/g, 'core-css', 'heading', transfromHeading))
     .pipe(replace(/\.h1, \.h2, \.h3, \.h4, \.h5, \.h6[\s\S]*?}\n}/g, ''))
-    .pipe(wait(500))
+    // .pipe(wait(500))
 
     // h1, h2, h3
     .pipe(extractSelector(/h1, h2, h3 [\s\S]*?}\n}/g, 'core-css', 'raw-text', prependHeaderRawText))
     .pipe(replace(/h1, h2, h3/g, '.h1, .h2, .h3'))
     .pipe(extractSelector(/\.h1, \.h2, \.h3[\s\S]*?}\n}/g, 'core-css', 'heading', transfromHeading))
     .pipe(replace(/\.h1, \.h2, \.h3[\s\S]*?}\n}/g, ''))
-    .pipe(wait(500))
+    // .pipe(wait(500))
 
     // h4, h5, h6
     .pipe(extractSelector(/h4, h5, h6 [\s\S]*?}\n}/g, 'core-css', 'raw-text', prependHeaderRawText))
     .pipe(replace(/h4, h5, h6/g, '.h4, .h5, .h6'))
     .pipe(extractSelector(/\.h4, \.h5, \.h6[\s\S]*?}\n}/g, 'core-css', 'heading', transfromHeading))
     .pipe(replace(/\.h4, \.h5, \.h6[\s\S]*?}\n}/g, ''))
-    .pipe(wait(500))
+    // .pipe(wait(500))
     /*
     */
 
@@ -213,22 +214,186 @@ gulp.task('copy-type', function() {
     .pipe(extractSelector(/\.heading_level_(1|2|3|4|5|6) [\s\S]*?}/g, 'core-css', 'heading'))
     .pipe(replace(/\.heading_level_(1|2|3|4|5|6) [\s\S]*?}/g, ''))
     .pipe(wait(500))
-    /*
-    */
 
-    /*
-    .pipe(replace(/\.img_(responsive|rounded|thumbnail|circle)_true[\s\S]*?}/g, ''))
-    .pipe(extractSelector(/\hr[\s\S]*?}/g, 'core-css', 'raw-text', prependRawText))
+    /**
+     * Body text
+     */
+    .pipe(extractSelector(/p {[\s\S]*?}/g, 'core-css', 'raw-text', prependRawText))
+    .pipe(replace(/p {/g, '.p {'))
+    .pipe(extractSelector(/\.p {[\s\S]*?}/g, 'core-css', 'p'))
+    .pipe(replace(/\.p {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
 
-    .pipe(replace(/\hr {/g, '.hr {'))
-    .pipe(extractSelector(/\.hr[\s\S]*?}/g, 'core-css', 'hr'))
-    .pipe(replace(/\.hr[\s\S]*?}/g, ''))
+    .pipe(extractSelector(/\.lead {[\s\S]*?}\n}/g, 'core-css', 'lead'))
+    .pipe(replace(/\.lead {[\s\S]*?}\n}/g, ''))
+    .pipe(wait(500))
 
-    .pipe(extractSelector(/\.sr-only {[\s\S]*?}/g, 'core-css', 'sr-only'))
-    .pipe(replace(/\.sr-only {[\s\S]*?}/g, ''))
-    .pipe(extractSelector(/\.sr-only-focusable {[\s\S]*}/g, 'core-css', 'sr-only'))
-    .pipe(replace(/\.sr-only-focusable {[\s\S]*}/g, ''))
-    */
+    /**
+     * Emphasis & misc
+     */
+
+    .pipe(replace(/\nsmall, \.small {/g, '\ntempsmall {'))
+    .pipe(extractSelector(/tempsmall {[\s\S]*?}/g, 'core-css', 'raw-text', function(item) {
+      return prependRawText(item.replace(/temp/g, ''));
+    }))
+    .pipe(extractSelector(/tempsmall {[\s\S]*?}/g, 'core-css', 'small', function(item) {
+      return item.replace(/tempsmall/g, '.small');
+    }))
+    .pipe(replace(/tempsmall {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/cite {[\s\S]*?}/g, 'core-css', 'raw-text', prependRawText))
+    .pipe(replace(/cite {/g, '.cite {'))
+    .pipe(extractSelector(/\.cite {[\s\S]*?}/g, 'core-css', 'cite'))
+    .pipe(replace(/\.cite {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(replace(/mark, .mark {/g, 'mark {'))
+    .pipe(extractSelector(/mark {[\s\S]*?}/g, 'core-css', 'raw-text', prependRawText))
+    .pipe(replace(/mark {/g, '.mark {'))
+    .pipe(extractSelector(/\.mark {[\s\S]*?}/g, 'core-css', 'mark'))
+    .pipe(replace(/\.mark {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.text-(left|right|center|justify|nowrap)[\s\S]*?}/g, 'core-css', 'text', function(item) {
+      return item.replace(/\.text-/g, '.text_align_');
+    }))
+    .pipe(replace(/\.text-(left|right|center|justify|nowrap)[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.text-(lowercase|uppercase|capitalize)[\s\S]*?}/g, 'core-css', 'text', function(item) {
+      return item.replace(/\.text-/g, '.text_case_');
+    }))
+    .pipe(replace(/\.text-(lowercase|uppercase|capitalize)[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.text-muted[\s\S]*?}/g, 'core-css', 'text', function(item) {
+      return item.replace(/\.text-muted/g, '.text_muted_true');
+    }))
+    .pipe(replace(/\.text-muted[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.text-(primary|success|info|warning|danger)[\s\S]*?}/g, 'core-css', 'text', function(item) {
+      return item.replace(/\.text-(primary|success|info|warning|danger)/g, '.text_theme_$1');
+    }))
+    .pipe(replace(/\.text-(primary|success|info|warning|danger)[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.bg-(primary|success|info|warning|danger)[\s\S]*?}/g, 'core-css', 'bg', function(item) {
+      return item.replace(/\.bg-(primary|success|info|warning|danger)/g, '.bg_theme_$1');
+    }))
+    .pipe(replace(/\.bg-(primary|success|info|warning|danger)[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    /**
+     * Page header
+     */
+    .pipe(extractSelector(/\.page-header[\s\S]*?}/g, 'core-css', 'page-header'))
+    .pipe(replace(/\.page-header[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    /**
+     * Lists
+     */
+    .pipe(extractSelector(/\nul, ol[\s\S]*?}\n}/g, 'core-css', 'raw-text', function(item) {
+      return item.replace(/\nul, ol/g, '.raw-text ul, .raw-text ol');
+    }))
+    .pipe(replace(/\nul, ol {/g, '\n.list {'))
+    .pipe(replace(/  ul, ol {/g, '  .list {'))
+    .pipe(extractSelector(/\.list {[\s\S]*?}\n}/g, 'core-css', 'list'))
+    .pipe(replace(/\.list {[\s\S]*?}\n}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.list-unstyled {[\s\S]*?}/g, 'core-css', 'list', function(item) {
+      return item.replace(/\.list-unstyled {/g, '.list_unstyled_true {');
+    }))
+    .pipe(replace(/\.list-unstyled {[\s\S]*?}/g, ''))
+    .pipe(replace(/\.list-unstyled/g, '.list_unstyled_true'))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.list-inline {[\s\S]*?}\n}/g, 'core-css', 'list', function(item) {
+      return item.replace(/\.list-inline {/g, '.list_inline_true {').replace(/> li/g, '.list__item');
+    }))
+    .pipe(replace(/\.list-inline {[\s\S]*?}\n}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/dl {[\s\S]*?}/g, 'core-css', 'raw-text', prependRawText))
+    .pipe(extractSelector(/dt, dd {[\s\S]*?}/g, 'core-css', 'raw-text', function(item) {
+      return item.replace(/dt, dd/g, '.raw-text dt, .raw-text dd');
+    }))
+    .pipe(extractSelector(/\ndt {[\s\S]*?}/g, 'core-css', 'raw-text', function(item) {
+      return prependRawText(item.replace(/\nd/g, 'd'));
+    }))
+    .pipe(extractSelector(/\ndd {[\s\S]*?}/g, 'core-css', 'raw-text', function(item) {
+      return prependRawText(item.replace(/\nd/g, 'd'));
+    }))
+
+    .pipe(replace(/dl {/g, '.dl {'))
+    .pipe(extractSelector(/\.dl {[\s\S]*?}/g, 'core-css', 'dl'))
+    .pipe(replace(/\.dl {[\s\S]*?}/g, ''))
+    .pipe(replace(/dt, dd {/g, '.dl__term, .dl__desc {'))
+    .pipe(extractSelector(/\.dl__term, .dl__desc {[\s\S]*?}/g, 'core-css', 'dl'))
+    .pipe(wait(500))
+
+    .pipe(replace(/\.dl__term, .dl__desc {[\s\S]*?}/g, ''))
+    .pipe(replace(/dt {/g, '.dl__term {'))
+    .pipe(replace(/dd {/g, '.dl__desc {'))
+    .pipe(extractSelector(/\.dl__term {[\s\S]*?}/, 'core-css', 'dl'))
+    .pipe(extractSelector(/\.dl__desc {[\s\S]*?}/, 'core-css', 'dl'))
+    .pipe(replace(/\.dl__term {[\s\S]*?}/, ''))
+    .pipe(replace(/\.dl__desc {[\s\S]*?}/, ''))
+    .pipe(wait(500))
+
+    .pipe(replace(/\.dl-horizontal {/g, '.dl_horizontal_true {'))
+    .pipe(extractSelector(/\.dl_horizontal_true {[\s\S]*?}\n}/g, 'core-css', 'dl'))
+    .pipe(replace(/\.dl_horizontal_true {[\s\S]*?}\n}/g, ''))
+    .pipe(wait(500))
+
+    /**
+     * Misc
+     */
+    .pipe(extractSelector(/abbr\[title\], \/\/[\s\S]*?\nabbr\[data-original-title\] {[\s\S]*?}/g, 'core-css', 'raw-text', function(item) {
+      return item.replace(/abbr\[/g, '.raw-text abbr[');
+    }))
+    .pipe(extractSelector(/abbr\[title\], \/\/[\s\S]*?\nabbr\[data-original-title\] {[\s\S]*?}/g, 'core-css', 'abbr', function(item) {
+      return item.replace(/abbr\[/g, '.abbr[');
+    }))
+    .pipe(replace(/abbr\[title\], \/\/[\s\S]*?\nabbr\[data-original-title\] {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/\.initialism {[\s\S]*?}/g, 'core-css', 'initialism'))
+    .pipe(replace(/\.initialism {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/blockquote {[\s\S]*?}\n}/, 'core-css', 'raw-text', prependRawText))
+    .pipe(extractSelector(/blockquote {[\s\S]*?}\n}/, 'core-css', 'blockquote', function(item) {
+      return item
+          .replace(/blockquote {/g, '.blockquote {')
+          .replace('p, ul, ol {', 'p, .p, ul, ol, .list {')
+          .replace('footer, small, .small {', 'footer, .footer, small, .small {');
+    }))
+    .pipe(replace(/blockquote {[\s\S]*?}\n}/, ''))
+    .pipe(wait(500))
+
+    .pipe(replace('.blockquote-reverse, blockquote.pull-right {', '.blockquote_reverse_true {'))
+    .pipe(extractSelector(/\.blockquote_reverse_true {[\s\S]*?}\n}/g, 'core-css', 'blockquote', function(item) {
+      return item.replace('footer, small, .small {', 'footer, .footer, small, .small {');
+    }))
+    .pipe(replace(/\.blockquote_reverse_true {[\s\S]*?}\n}/g, ''))
+
+    .pipe(extractSelector(/blockquote:before, blockquote:after {[\s\S]*?}/g, 'core-css', 'raw-text', function(item) {
+      return item.replace(/blockquote/g, '.raw-text blockquote');
+    }))
+    .pipe(extractSelector(/blockquote:before, blockquote:after {[\s\S]*?}/g, 'core-css', 'blockquote', function(item) {
+      return item.replace(/blockquote/g, '.blockquote');
+    }))
+    .pipe(replace(/blockquote:before, blockquote:after {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
+
+    .pipe(extractSelector(/address {[\s\S]*?}/g, 'core-css', 'raw-text', prependRawText))
+    .pipe(extractSelector(/address {[\s\S]*?}/g, 'core-css', 'address'))
+    .pipe(replace(/address {[\s\S]*?}/g, ''))
+    .pipe(wait(500))
 
     .pipe(rename(prependFilename))
     .pipe(gulp.dest('levels/core-css'));
@@ -295,14 +460,11 @@ var extractSelector = function(reg, level, block, transform) {
 
     var contents = file.contents.toString('utf8');
     var res = contents.match(reg);
+    var appendNL = function(item) { return item + '\n'; };
     vfs.makeDir(folder).then(function() {
       if (res && res.length > 0) {
-        res.forEach(function(data) {
-          vfs.append(filename, transform(data) + '\n', console.log);
-        });
-
+        vfs.append(filename, res.map(transform).map(appendNL).join(''), console.log);
       }
-
     });
 
     this.push(file);
